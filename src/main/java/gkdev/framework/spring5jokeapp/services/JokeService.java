58b889bf72c9debafac69d0c10ecd109298045b7
Konstantin Romanov <konstantin.romanov@live.be>
2020-03-30 11:31:28 +0200

    Creating Spring Service Layer: JokeService

---
+++ src/main/java/gkdev/framework/spring5jokeapp/services/JokeService.java
@@ -0,0 +1,11 @@
+package gkdev.framework.spring5jokeapp.services;
+
+/**
+ * @author Konstantin Romanov
+ * @created 30/03/2020 - 11:25
+ * @project spring5-jokeapp
+ */
+public interface JokeService {
+
+    String getJoke();
+}
---
+++ src/main/java/gkdev/framework/spring5jokeapp/services/JokeServiceImpl.java
@@ -0,0 +1,24 @@
+package gkdev.framework.spring5jokeapp.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Konstantin Romanov
+ * @created 30/03/2020 - 11:26
+ * @project spring5-jokeapp
+ */
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return this.chuckNorrisQuotes.getRandomQuote();
+    }
+}
