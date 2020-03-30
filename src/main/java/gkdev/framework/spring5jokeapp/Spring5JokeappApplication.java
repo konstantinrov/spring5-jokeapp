b603f63762d2a85c5a6c2b2305421933f5bfdc18
Konstantin Romanov <konstantin.romanov@live.be>
2020-03-30 11:19:49 +0200

    Initial Project Add.

---
+++ src/main/java/gkdev/framework/spring5jokeapp/Spring5JokeappApplication.java
@@ -0,0 +1,13 @@
+package gkdev.framework.spring5jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokeappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gkdev/framework/spring5jokeapp/Spring5JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package gkdev.framework.spring5jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
